53bf8d1eac9f09b4d2614b49a750f79384ed3bde
felegare <flix.lgar@gmail.com>
2020-03-28 16:03:34 -0400

    Add exif tags editing function to insert GPS coordinates (latitude, longitude, altitude) into picture's metadata

--- PiCam.py
+++ PiCam.py
@@ -1,8 +1,9 @@
-from time import sleep
+import time
 from picamera import PiCamera
 import adafruit_gps
 import serial
 import time
+from GPSPhoto import gpsphoto
 from pathlib import Path
 
 
@@ -23,7 +24,7 @@ def get_coords():
 		gps.update()
 		
 		if len(str(gps.latitude).split('.')[-1]) > 5 and len(str(gps.longitude).split('.')[-1]) > 5:
-			return gps.latitude, gps.longitude
+			return gps.latitude, gps.longitude, gps.altitude_m
 			
 			break
 	
@@ -35,35 +36,49 @@ def timestamp():
 	
 	'''
 	timestamp = time.strftime("%H%M%S",time.gmtime())
-	filename = 'img_'+ timestamp + '.jpg'
+	filename = 'img_' + timestamp + '.jpg'
 	
 	return filename
 
 
-def take_picture(folder_name, file_name):
+def take_picture(path):
 	'''
 	Takes picture with Raspberry Pi Camera Module
-	:param folder : (str) name of dedicated image folder for pictures
-	:param name : (str) filename given by the timestamp function
+	:param path : (str) path to picture
 	
 	'''
 	camera = PiCamera()
 	camera.resolution = (1280, 720)
-	camera.capture(folder_name + '/' + file_name)
+	camera.capture(path)
+	
+def edit_exif(path, gps_data):
+	'''
+	Takes gps coordinates and inserts them to the picture's metadata
+	:param path : (str) path to picture
+	:parma gps_data : (tuple) (latitude, longitude, altitude)
+	
+	'''
+	photo = gpsphoto.GPSPhoto(path)
+	info = gpsphoto.GPSInfo((gps_data[0], gps_data[1]), int(gps_data[2]))
+	photo.modGPSData(info, path)
 	
 	
 def main():
 	date = time.strftime("%Y-%m-%d", time.gmtime())
-	folder = "IMG/" + date
+	folder = 'IMG/' + date
 	
 	# Creates a directory for pictures based on the current date
 	Path(folder).mkdir(parents=True, exist_ok=True)
 	
-	print(get_coords())
+	coords = get_coords()
 	
 	filename = timestamp()
-	take_picture(folder, filename)
+	picture_path = folder + '/' + filename
+	
+	take_picture(picture_path)
+	
+	edit_exif(picture_path, coords)
 	
 
 if __name__ == '__main__':
-   main()
+	main()
