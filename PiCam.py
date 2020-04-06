1db460cc3a821161ef16bc865858a9794bb36124
felegare <flix.lgar@gmail.com>
2020-04-06 19:43:19 -0400

    Add mean to handle and show when GPS gets NO FIX

--- PiCam.py
+++ PiCam.py
@@ -3,23 +3,16 @@ import RPi.GPIO as GPIO
 from picamera import PiCamera
 import adafruit_gps
 import serial
-import time
 from GPSPhoto import gpsphoto
 from pathlib import Path
 
 
-def get_coords():
+def get_coords(gps):
 	'''
 	Get GPS coordinates with adafruit GPS module
 	:return : (tuple) latitude and longitude coordinates
 	
 	'''
-	
-	coords = {}
-	
-	# for a computer, use the pyserial library for uart access
-	uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
-	gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
 	
 	while True:
 		gps.update()
@@ -46,6 +39,10 @@ def main():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	
+	# for a computer, use the pyserial library for uart access
+	uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
+	gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
+	
 	# Camera settings
 	camera = PiCamera()
 	camera.resolution = (1280, 720) # Pictures' resolution : 720p
@@ -53,34 +50,51 @@ def main():
 	# Preview window settings
 	preview = camera.start_preview()
 	preview.fullscreen = False
-	preview.window = (0, 0, 480, 320) # Resolution of the screen
+	preview.window = (0, -25, 480, 320) # Resolution of the screen
+	
+	time.sleep(1)
 
 	while True:
 		input_state = GPIO.input(21)
 		
-		# If button is pressed
-		if input_state == False:
+		gps.update()
+		
+		if gps.has_fix:
 			
-			# Stores the date and time
-			current_date = time.strftime("%Y-%m-%d", time.localtime())
-			current_time = time.strftime("%H%M%S", time.localtime())
+			camera.annotate_text=' '
+		
+			# If button is pressed
+			if input_state == False:
+				
+				# Stores the date and time
+				current_date = time.strftime("%Y-%m-%d", time.localtime())
+				current_time = time.strftime("%H%M%S", time.localtime())
+				
+				# Creates a directory for pictures based on the current date
+				Path(f'IMG/{current_date}').mkdir(parents=True, exist_ok=True)
+				
+				# Gets the longitude and latitude coordinates frome the GPS module
+				coords = get_coords(gps)
+		
+				# Creates unique path for the picture from the date and time
+				picture_path = f'IMG/{current_date}/IMG_{current_time}.jpeg'
+		
+				# Takes the picture
+				camera.capture(picture_path)
+				
+				# Writes the coordinates to the picture's EXIF tags
+				# Saves the pictures
+				edit_exif(picture_path, coords)
 			
-			# Creates a directory for pictures based on the current date
-			Path(f'IMG/{current_date}').mkdir(parents=True, exist_ok=True)
+			continue
+				
+		elif not gps.has_fix:
 			
-			# Gets the longitude and latitude coordinates frome the GPS module
-			coords = get_coords()
-	
-			# Creates unique path for the picture from the date and time
-			picture_path = f'IMG/{current_date}/IMG_{current_time}.jpeg'
-	
-			# Takes the picture
-			camera.capture(picture_path)
+			# Display 'NO FIX' message over preview
+			camera.annotate_text=f'NO FIX - {gps.satellites} SATELLITES'
 			
-			# Writes the coordinates to the picture's EXIF tags
-			# Saves the pictures
-			edit_exif(picture_path, coords)
-	
+			continue
+			
 
 if __name__ == '__main__':
 	main()
